3316caef751f0730a3dec237a580592fd6a71461
ienix <guoaimin01@baidu.com>
2017-11-06 20:08:05 +0800

    bugfixed

--- command/doc/index.js
+++ command/doc/index.js
@@ -11,136 +11,47 @@
 
 const PATH = require('path');
 const FS = require('fs');
-const EXEC = require('child_process').exec;
-const SPAWN = require('child_process').spawn;
-const OS = require('os');
-
-const PROGRAM = require('commander');
-const CO = require('co');
 const RUN = require('exec-cmd');
-
-const PROGRESS = require('../../util/progress')();
 
 const LOG = require('../../util/log');
 
-const TARGET_DIR = PATH.join(process.cwd());
+const ETPL = require('etpl');
+
 const DOC_DIR = PATH.join('./', 'doc');
 const DOC_SOURCE = PATH.join(DOC_DIR, '/source');
-const GITBOOK_DIR = PATH.join(DOC_SOURCE, '/gitbook');
 
-const CREATE_SUMMARY = require('./summary');
+let summary = {};
+module.exports = fileTree => {
 
-let fileTree = [];
-let createGitBookCopy = path => {
-    path = path || TARGET_DIR;
+    fileTree.map(item => {
+        let path = item.split('/')
+            .slice(1, -1);
+        let key = path[0];
 
-    FS.readdirSync(path)
-        .filter(item => !/^\./i.test(item))
-        .sort()
-        .forEach(item => {
-            let target = PATH.join(path, item);
-            let isDir = false;
-
-            try {
-                isDir = FS.statSync(target).isDirectory();
-            }
-            catch(e) {}
-
-            if (isDir) {
-
-                createGitBookCopy(target);
-            }
-            else if (/.+\.md$/gi.test(target)) {
-                fileTree.push(target);
-                moveFile(target);
-            }
-        });
-};
-let moveFile = (target) => {
-    let path = PATH.join(GITBOOK_DIR, '/', target);
-    let text = FS.readFileSync(target);
-    let dir = path.slice(0, path.lastIndexOf('/'));
-
-    RUN('mkdir', ['-p', dir])
-        .then(() => {
-            FS.writeFileSync(path, text);
-        })
-        .catch(error => {
-            LOG(`${error}!`);
-        });
-};
-let createGitBook = () => {
-    if (FS.existsSync(DOC_DIR)) {
-        RUN('rm', ['-rf', 'DOC_DIR']);
-    }
-
-    try {
-        createGitBookCopy();
-        CREATE_SUMMARY(fileTree);
-    }
-    catch (error) {
-        LOG(`${error.message}`, 'red');
-    }
-
-    let command = [];
-
-    command.push(`cd ${DOC_SOURCE}`);
-    command.push('gitbook build');
-
-    EXEC(command.join(' && '), (error, stdout, stderr) => {
-        if (error) {
-            LOG(`${error[0]}`);
+        if (!summary[key]) {
+            summary[key] = [];
         }
+        let value = path.slice(1);
 
-        console.log(stdout, stderr);
+        value.length && summary[key].push(value);
 
-        PROGRESS.clear();
-        LOG('Generation documentation completed!', 'success');
+        return path;
     });
-};
 
-module.exports = () => {
-    CO(function *() {
-        if (!FS.existsSync(TARGET_DIR)) {
-            LOG('The `Target dir` directory is not exist!');
-        }
+    let path = PATH.join(__dirname, '../../template/doc/gitbook.html');
+    let template = FS.readFileSync(path, 'utf8');
+    let render = ETPL.compile(template);
+    let text = render({data: summary, url: 'gitbook/components/'});
 
-        let args = PROGRAM.args[0];
-        let gitbook = args.gitbook;
+    text = text.replace(/^[\s\t]*(\r\n|\n|\r)/gm,'');
 
-        // create doc!
+    RUN('cp', ['./README.md', `${DOC_SOURCE}/README.md`])
+        .catch(response => {
+            LOG(`${response[0]}`);
+        });
 
-        PROGRESS.set('text', 'Start generating...')
-            .start();
+    FS.writeFileSync(`${DOC_SOURCE}/SUMMARY.md`, text);
 
-        if (gitbook) {
-            RUN('gitbook', ['-h'])
-                .catch(error => {
-                    let message = 'The gitbook command is not exit! \n Please run `npm i gitbook-cli -g` !';
-
-                    console.log(error);
-
-                    LOG(message);
-                });
-            createGitBook();
-        }
-
-        let server = args.server;
-
-        if (server) {
-            let commmand = OS.platform() === 'win32'
-                ? `${__dirname}/doc.cmd`
-                : ['sh', [`${__dirname}/doc.sh`]];
-
-            let gitbookServer = SPAWN(...commmand);
-
-            gitbookServer.stdout.on('data', (data) => {
-                console.log(data.toString());
-            });
-
-            gitbookServer.stderr.on('data', (data) => {
-                console.log(data.toString());
-            });
-        }
-    });
+    LOG('√ Generation summary completed!', 'green');
+    LOG('Wait for a moment! Doc generating...', 'gray');
 };
